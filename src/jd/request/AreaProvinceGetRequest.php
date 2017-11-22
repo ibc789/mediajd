0ec62664f16de69967eca83c766baacb1300f0d6
ibc789@163.com <870812ibc>
2017-11-22 15:53:49 +0800

    no message

---
+++ src/jd/request/AreaProvinceGetRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Ibc\MediaJd;
+class AreaProvinceGetRequest
+{
+	private $apiParas = array();
+	
+	public function getApiMethodName(){
+	  return "jingdong.area.province.get";
+	}
+	
+	public function getApiParas(){
+		return json_encode($this->apiParas);
+	}
+	
+	public function check(){
+		
+	}
+	
+	public function putOtherTextParam($key, $value){
+		$this->apiParas[$key] = $value;
+		$this->$key = $value;
+	}
+    }
+
+
+
+
+
+        
+ 
+
---
+++ src/jd/request/UnionDoubanBookServiceQueryBookUrlListRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Ibc\MediaJd;
+class UnionDoubanBookServiceQueryBookUrlListRequest
+{
+	private $apiParas = array();
+	
+	public function getApiMethodName(){
+	  return "jingdong.UnionDoubanBookService.queryBookUrlList";
+	}
+	
+	public function getApiParas(){
+		return json_encode($this->apiParas);
+	}
+	
+	public function check(){
+		
+	}
+	
+	public function putOtherTextParam($key, $value){
+		$this->apiParas[$key] = $value;
+		$this->$key = $value;
+	}
+                                    	}
+
+
+
+
+
+        
+ 
+
